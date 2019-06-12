0362a550acb764901c7f5ac3d0b97b95c9cf62eb
William Soukkachang <WSoukkachang@yahoo.com>
2019-06-11 23:08:46 -0500

    Added seed for cohorts. Finished GET, PUT, DELETE methods. Completed MVP. Working on strech.

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-//end points
+// END POINTS //
 
-// POST
+// POST IN COHORTS
 
 server.post('/api/cohorts', async (req, res) => {
     try {
@@ -24,7 +24,7 @@ server.post('/api/cohorts', async (req, res) => {
     }
 });
 
-// GET
+// GET ALL COHORTS
 
 server.get('/api/cohorts', async (req, res) => {
     try {
@@ -34,6 +34,59 @@ server.get('/api/cohorts', async (req, res) => {
     res.status(500).json({ error: "There was an error retrieving data" });
     }
 });
+
+// GET COHORT WITH MATCHING ID
+
+server.get('/api/cohorts/:id', async (req, res) => {
+    try {
+    const cohort = await db('cohorts').where({ id: req.params.id }).first();
+    res.status(200).json(cohort);
+    } catch {
+    res.status(500).json({ error: "There was an error retrieving data"});
+    }
+})
+
+// GET ALL STUDENTS FOR COHORT WITH MATCHING ID
+
+server.get('/api/cohorts/:id/students', async (req, res) => {
+    try {
+    const students = await db('students').where({ cohorts_id: req.params.id })
+    res.status(200).json(students);
+    } catch {
+    res.status(500).json({ error: "There was an error retrieving student data for that cohort" })
+    }
+})
+
+// PUT OR UPDATE COHORT DATA WITH MATCHING ID
+
+server.put('/api/cohorts/:id', async (req, res) => {
+    try {
+    const count = await db('cohorts').where({ id: req.params.id }).update(req.body);
+    if(count > 0) {
+        res.status(200).json({ message: `${count} cohort record updated`})
+    } else {
+        res.status(404).json({ message: "Cohort by ID not found"})
+    }
+    } catch {
+    res.status(500).json({ error: "There was an error updating data" })
+    }
+})
+
+// DELETE BY ID
+
+server.delete('/api/cohorts/:id', async (req, res) => {
+    try {
+    const count = await db('cohorts').where({ id: req.params.id }).del();
+    if(count > 0) {
+        res.status(200).json({ message: `${count} cohort record deleted`})
+    } else {
+        res.status(404).json({ message: "Cohort by ID not found"})
+    }
+    } catch {
+    res.status(500).json({ error: "There was an error deleting data" })
+    }
+})
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
